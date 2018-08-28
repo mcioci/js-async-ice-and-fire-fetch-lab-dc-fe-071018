f8cb09db87a874ff471400d51f9653e814402246
meghan.cioci@wework.com <meghan.cioci@wework.com>
2018-08-28 23:12:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,39 +1,8 @@
-
-  .then( resp => "https://anapioficeandfire.com/api/houses")
+function fetchBooks("https://anapioficeandfire.com/api/books") {
+  .then(resp => resp.json())
+  .then(json => console.log(json));
   //write fetch request to the Game of Thrones API
 }
-
-function fetchBooks("books") {
-        console.log("== fetchBooks ==");
-        for (i = 0; i < books.length; i++) {
-          console.log("books[i].name:", books[i].name);
-        }
-    }
-
-    fetch("https://anapioficeandfire.com/api/books")
-      .then( (resp) => {
-        console.log("== then1 ==");
-        return resp.json()
-      })
-      .catch( error => {
-        console.log("== catch1 ==");
-        console.error(`Oh no! ${error}`)
-      })
-      .then( json => {
-        console.log("== then2 ==");
-        console.log(json);
-        return json
-      } )
-      .catch( error => {
-        console.log("== catch2 ==");
-        console.error(`Ruh-roh! Couldn't convert the json: ${error}`)
-      })
-      .then( json => {
-        console.log("== then3 ==");
-        console.log("json.books", json.books);
-        fetchBooks(json.books);
-      } )
-
 
 function renderBooks(json) {
   const main = document.querySelector('main')
